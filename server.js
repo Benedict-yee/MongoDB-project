634536d6b6dd6066fa5db31b39306194adb7a33a
Benedict-yee <60129385+Benedict-yee@users.noreply.github.com>
2021-04-16 03:10:58 -0400

    General Style

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const express = require('express')
 const app = express()
 const expressLayouts=require('express-ejs-layouts')
 const bodyParser=require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter=require('./routes/index')
 const authorRouter=require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
@@ -16,6 +18,7 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(methodOverride('_method'))
 
 const mongoose = require ('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -29,5 +32,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
