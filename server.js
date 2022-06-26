0ab0678edfd60f7aa46c1f8bf8229bc8bb772684
Benedict-yee <60129385+Benedict-yee@users.noreply.github.com>
2022-06-26 04:18:38 -0400

    server.js partial modify

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
-
-const express = require('express')
-const app = express()
-const expressLayouts=require('express-ejs-layouts')
+// express is a node.js webapp framework
+const express = require('express') // import express from the express library that we installed npm
+const app = express() // call express function to get the app portion 
+const expressLayouts=require('express-ejs-layouts') // import express layout that we installed
 const bodyParser=require('body-parser')
 const methodOverride = require('method-override')
 
-const indexRouter=require('./routes/index')
+const indexRouter=require('./routes/index') // import index router
 const authorRouter=require('./routes/authors')
 const bookRouter = require('./routes/books')
 
 
-app.set('view engine','ejs')
-app.set('views',__dirname+'/views')
-app.set('layout','layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+app.set('view engine','ejs') // we use 'ejs' as our view engine, so pass in 'ejs'
+app.set('views',__dirname+'/views') // we set where the 'views' are coming from. __dirname: current directory
+app.set('layout','layouts/layout') // we hook up express layout.
+app.use(expressLayouts) // we tell the express app that we want to use express layouts
+app.use(express.static('public')) // we tell the express app where our public files are going to be
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 app.use(methodOverride('_method'))
 
-const mongoose = require ('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser:true, useUnifiedTopology:true})
-    .then(() => {
-        console.log("Connected to MongoDB");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+const mongoose = require ('mongoose') // import mongoose
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use('/',indexRouter)
+
+app.use('/',indexRouter) // use index router
 app.use('/authors',authorRouter)
 app.use('/books', bookRouter)
 
